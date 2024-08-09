670be5ecc98219af80534225c23b99419c5522fb
W.Xavier <wellington222688@gmail.com>
2024-08-09 17:09:30 -0400

    basico

---
+++ java-anatomia-classes/src/edu/xavier/basicos/somadoisNumeros.java
@@ -0,0 +1,8 @@
+public class Soma {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int soma = num1 + num2;
+        System.out.println("A soma de " + num1 + " e " + num2 + " é " + soma);
+    }
+}
